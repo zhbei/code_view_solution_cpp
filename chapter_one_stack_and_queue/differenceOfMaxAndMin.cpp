5031100019cd1af0a0a90b227c2f353223413e3b
zhbei <zhangbeibei@ict.ac.cn>
2018-09-02 11:31:19 +0800

    zhbei

--- chapter_one_stack_and_queue/differenceOfMaxAndMin.cpp
+++ chapter_one_stack_and_queue/differenceOfMaxAndMin.cpp
@@ -11,40 +11,43 @@ int getNumber(const vector<int> &nums,const int diff){
 	int ans = 0;
 
 	deque<int> maxD,minD;
-
+	vector<bool> visited(size,false);
 	int i = 0,j = 0;
 	while(i < size){
+		//there are some question
+		// some index maybe add to minD or maxD more than onece 
+		// how can ensure this situation will not be encour mistake??
+		//for fix this question I new a visited array to record which index is dealed
 		while(j < size){
-			while(!maxD.empty() && nums[maxD.back()] <= nums[j]){
-				maxD.pop_back();
+			if(visited[j] == false){
+				while(!maxD.empty() && nums[maxD.back()] <= nums[j]){
+					maxD.pop_back();
+				}
+				maxD.push_back(j);
+				while(!minD.empty() && nums[minD.back()] >= nums[j]){
+					minD.pop_back();
+				}
+				minD.push_back(j);
+				visited[j] = true;
 			}
-			maxD.push_back(j);
-			while(!minD.empty() && nums[minD.back()] >= nums[j]){
-				minD.pop_back();
-			}
-			minD.push_back(j);
-
-			++j;
 			if(nums[maxD.front()] - nums[minD.front()] > diff){
-				ans += j - i - 1;
-				cout << j - i - 1 << endl;
 				break;
 			}
+			++j;
 		}
-		if(nums[maxD.front()] - nums[minD.front()] <= diff){
-			ans += j - i;
-			cout << j - i << endl;
-			++i;
+		if(maxD.front() == i){
+			maxD.pop_front();
 		}
-		while(!maxD.empty() && !minD.empty() && (nums[maxD.front()] - nums[minD.front()]) > diff){
-			if(maxD.front() == i){
-				maxD.pop_front();
-			}
-			if(minD.front() == i){
-				minD.pop_front();
-			}
-			++i;
+		if(minD.front() == i){
+			minD.pop_front();
 		}
+		//if a[i...j] is vailid , then a[i+k...j] is also vailid
+		//because when the i is plus some number, there are two situation 
+		// 1. maxVal will be decrease or
+		// 2. minVal will be increase 
+		// In either case the difference of maxVal and minVal will be decrease
+		ans += j - i;
+		++i;
 	}
 	return ans;
 }
@@ -74,8 +77,9 @@ int validate(const vector<int> &nums,const int diff){
 
 int main(int argc, char const *argv[])
 {
-	vector<int> arr{4,5,6,7,1,2,4,1,3,13,7};
-	cout << "Ans should be : " << validate(arr,10) << endl;
-	cout << "My ans is : " << getNumber(arr,10) << endl;
+	vector<int> arr{1,2,3,4,1,1,2,1,3};
+	int diff = 3;
+	cout << "Ans should be : " << validate(arr,diff) << endl;
+	cout << "My ans is : " << getNumber(arr,diff) << endl;
 	return 0;
 }
